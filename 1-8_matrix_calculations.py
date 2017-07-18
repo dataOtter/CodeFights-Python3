01cdf4f362970ea0b07e49c72e03e2407388fa5c
DESKTOP-UR6OLHP\Maisha <maisha.jauernig@gmail.com>
2017-07-18 18:54:13 -0400

    Working on problem 1-8 and 1-9.

--- 1-8_matrix_calculations.py
+++ 1-8_matrix_calculations.py
@@ -7,38 +7,28 @@ or is located anywhere below a free room in the same column is not considered su
 Help the bots calculate the total price of all the rooms that are suitable for them."""
 
 
-def matrixElementsSum(matrix):
+def matrix_elements_sum(matrix):
     """Input: 2-dimensional array of integers representing a rectangular matrix of the building.
     Guaranteed constraints: 1 ≤ matrix.length ≤ 5, 1 ≤ matrix[i].length ≤ 5, 0 ≤ matrix[i][j] ≤ 10.
     Output: Returns the sum (total price) of all eligible room prices."""
 
-    # Between 1 and 5 rows
-    # Between 1 and 5 columns/entries per row
-    # Max value of entry is 10, min 0
-
-    total_price = 0
-    prev_indices_to_avoid = []
-    indices_to_avoid = []
-    to_subtract = 0
+    indices_to_add = []
+    sum_price = sum(map(sum, matrix))
 
     for row_index in range(len(matrix)):
-        total_price += sum(matrix[row_index])
-        print(prev_indices_to_avoid)
+        #print("current total: " + str(sum_price))
+        #print(row_index, set(indices_to_add))
+        for column_index in set(indices_to_add):
+            sum_price -= matrix[row_index][column_index]
 
         for index, element in enumerate(matrix[row_index]):
             if element == 0:
-                print(index)
-                indices_to_avoid.append(index)
+                indices_to_add.append(index)
 
-        prev_indices_to_avoid = indices_to_avoid.copy()
-        indices_to_avoid.clear()
-
-    print(total_price)
+    return sum_price
 
 
-matrix = [[0, 1, 1, 2],
-          [0, 5, 0, 0],
-          [2, 0, 3, 3]]
+matrix = [[0]]
 
-matrixElementsSum(matrix)
+print(matrix_elements_sum(matrix))
 
---
+++ 1-9_longest_strings.py
@@ -0,0 +1,31 @@
+"""Given an array of strings, return another array containing all of its longest strings."""
+
+
+def all_longest_strings(input_array):
+    """A non-empty array. Guaranteed constraints: 1 ≤ inputArray.length ≤ 10, 1 ≤ inputArray[i].length ≤ 10."""
+    longest_length = 0
+    longest_strings = []
+
+    for element in input_array:
+        length = len(element)
+
+        if length == longest_length:
+            longest_strings.append(element)
+
+        elif length > longest_length:
+            longest_strings.clear()
+            longest_strings.append(element)
+            longest_length = length
+
+    return longest_strings
+
+
+a = ["a",
+ "abc",
+ "cbd",
+ "zzzzzz",
+ "a",
+ "abcdef",
+ "asasa",
+ "aaaaaa"]
+print(allLongestStrings(a))
